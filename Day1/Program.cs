40a100c30882385cbd44242542b3bca34c1a2827
patel <patel@DESKTOP-JQ3SQMP>
2025-01-27 12:31:51 +0530

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using Day1;
+using System;
+
+task1 t1= new task1();
+
+Console.WriteLine("Enter the number up to which you want to find the count of odd and even numbers.");
+
+int number=Convert.ToInt32(Console.ReadLine());
+
+t1.countnum(number);
---
+++ Day1/task1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public class task1
+    {   
+
+        public void countnum(int num)
+        {
+            int oddcount = 0;
+            int evencount = 0;
+            for (int i = 1; i <= num; i++)
+            {
+             if(i % 2 == 0)
+                {
+                    evencount++;
+                }
+                else
+                {
+                    oddcount++;
+                }
+
+            }
+            Console.WriteLine($"The odd-count till the {num} is {oddcount} ");
+            Console.WriteLine($"The even-count till the {num} is {evencount} ");
+        }
+
+    }
+}
