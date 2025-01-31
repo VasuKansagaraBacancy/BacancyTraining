335252ba70bf9a77234288c51b0de223a75b97e2
VasuKansagaraBacancy <vasu.kansagara@bacancy.com>
2025-01-31 17:36:05 +0530

    Day 4 task added

---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,22 @@
+﻿using Day4;
+
+task1 account = new task1(1000);
+Thread[] threads = new Thread[5];
+
+for (int i = 0; i < threads.Length; i++)
+{
+    threads[i] = new Thread(() =>
+    {
+        account.Deposit(500);
+        account.Withdraw(300);
+    });
+    threads[i].Name = $"Thread-{i + 1}";
+}
+
+foreach (var thread in threads)
+    thread.Start();
+
+foreach (var thread in threads)
+    thread.Join();
+
+Console.WriteLine($"Final Account Balance: {account.GetBalance()}");
---
+++ Day4/Day4/task1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    public class task1
+    {
+        private int balance;
+        private readonly object balanceLock = new object();
+
+        public task1(int initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public void Deposit(int amount)
+        {
+            lock (balanceLock)
+            {
+                balance += amount;
+                Console.WriteLine($"{Thread.CurrentThread.Name} deposited {amount}. New balance: {balance}");
+            }
+        }
+
+        public void Withdraw(int amount)
+        {
+            lock (balanceLock)
+            {
+                if (amount <= balance)
+                {
+                    balance -= amount;
+                    Console.WriteLine($"{Thread.CurrentThread.Name} withdrew {amount}. New balance: {balance}");
+                }
+                else
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.Name} attempted to withdraw {amount}, but insufficient balance.");
+                }
+            }
+        }
+
+        public int GetBalance()
+        {
+            lock (balanceLock)
+            {
+                return balance;
+            }
+        }
+    }
+
+    
+}
+
+
