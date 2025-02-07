56a605a957df405f292bd2d2c91edeb4a6b07a0d
VasuKansagaraBacancy <vasu.kansagara@bacancy.com>
2025-02-07 14:48:57 +0530

    Updated

--- OOPS_Day3/OOPS_Day3/Program.cs
+++ OOPS_Day3/OOPS_Day3/Program.cs
@@ -6,28 +6,30 @@ class Program
     static void Main()
     {
         INotification notification;
-        Console.Write("Enter notification type (Email/SMS/Push): ");
         try
         {
-            string type = Console.ReadLine();
-            switch (type)
+            while (true)
             {
-                case "Email":
-                    notification = new EmailNotification();
-                    break;
-                case "SMS":
-                    notification = new SmsNotification();
-                    break;
-                case "Push":
-                    notification = new PushNotification();
-                    break;
-                default:
-                    Console.WriteLine("Invalid notification type");
-                    return;
-            }
-
-            NotificationService service = new NotificationService(notification);
-            service.Notify("Brother, this is your notification!");
+                Console.Write("Enter notification type (Email/SMS/Push): ");
+                string type = Console.ReadLine();
+                switch (type)
+                {
+                    case "Email":
+                        notification = new EmailNotification();
+                        break;
+                    case "SMS":
+                        notification = new SmsNotification();
+                        break;
+                    case "Push":
+                        notification = new PushNotification();
+                        break;                  
+                    default:
+                        Console.WriteLine("Invalid notification type");
+                        return;
+                }
+                NotificationService service = new NotificationService(notification);
+                service.Notify("Brother, this is your notification!");
+            }           
         }
         catch(Exception ex) 
         {
