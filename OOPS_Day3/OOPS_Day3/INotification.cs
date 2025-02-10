09081879d9e5adb7b7cda0ceb1762253de761168
VasuKansagaraBacancy <vasu.kansagara@bacancy.com>
2025-02-10 11:15:56 +0530

    Updated

--- OOPS_Day3/OOPS_Day3/INotification.cs
+++ OOPS_Day3/OOPS_Day3/INotification.cs
@@ -31,16 +31,4 @@ namespace OOPS_Day3
             }
         }
     }
-    class NotificationService
-    {
-        private readonly INotification _notification;
-        public NotificationService(INotification notification)
-        {
-            _notification = notification;
-        }
-        public void Notify(string message)
-        {
-            _notification.send(message);
-        }
-    }
 }
---
+++ OOPS_Day3/OOPS_Day3/Notificationservice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Day3
+{
+    class NotificationService
+    {
+        private readonly INotification _notification;
+        public NotificationService(INotification notification)
+        {
+            _notification = notification;
+        }
+        public void Notify(string message)
+        {
+            _notification.send(message);
+        }
+    }
+}
