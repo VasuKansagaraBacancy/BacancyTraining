89aca7be30f15746ab123b4d393b376df2a6da37
VasuKansagaraBacancy <vasu.kansagara@bacancy.com>
2025-02-18 16:23:05 +0530

    Open Weather api Used

---
+++ DOTNET_Day1/DOTNET_Day1/Controllers/WeatherController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DOTNET_Day1.Controllers
+{
+
+    [Route("ap1/[controller]")]
+    [ApiController]
+    public class LocationAPI : Controller
+    {
+        [HttpPost]
+            public IActionResult WeatherPost([FromBody] LocationRequest request)
+            {
+                if (!OpenWeather.StationDictionary.TryGetClosestStation(request.Lat, request.Long, out var stationInfo))
+                {
+                    Console.WriteLine("Could not find a station.");
+                    return NotFound("ERROR");
+                }
+
+                var weather = new LocationInfo
+                {
+                    Name = stationInfo.Name,
+                    ICAO = stationInfo.ICAO,
+                    Lat = stationInfo.Latitude,
+                    Long = stationInfo.Longitude,
+                    Elevation = stationInfo.Elevation,
+                    Country = stationInfo.Country,
+                };
+
+                return Ok(weather.ToString());
+            }
+
+        }
+    
+}
---
+++ DOTNET_Day1/DOTNET_Day1/LocationRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace DOTNET_Day1
+{
+    public class LocationRequest
+    {
+            public double Lat { get; set; }
+            public double Long { get; set; }
+    }
+        public class LocationInfo
+        {
+            public String? Name;
+            public String? ICAO;
+            public double Lat;
+            public double Long;
+            public int? Elevation;
+            public String? Country;
+            public override string ToString()
+            {
+                return $"Name: {Name}, ICAO: {ICAO}, Lat: {Lat}, Long: {Long}, Elevation: {Elevation}, Country: {Country}";
+            }
+        
+}
+}
