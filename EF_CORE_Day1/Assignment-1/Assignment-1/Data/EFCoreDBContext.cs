5641d5bf931d0af8005c4203103cb7092f4c77eb
VasuKansagaraBacancy <vasu.kansagara@bacancy.com>
2025-03-06 14:25:21 +0530

    EF_CORE Standard Method

---
+++ EF_CORE_Day1/Assignment-1/Assignment-1/Data/EFCoreDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Assignment_1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment_1.Data
+{
+    public class EFCoreDBContext : DbContext
+    {
+        public EFCoreDBContext(DbContextOptions<EFCoreDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
