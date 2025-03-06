a3817fb1b51322895e11d6ec0c7cb3d33ee8a6a0
Vasu Kansagara <vasu.kansagara@bacancy.com>
2025-03-06 16:25:31 +0530

    Delete DOTNET_Day5 directory

--- DOTNET_Day5/DOTNET_Day5/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DOTNET_Day5
-{
-    public class Student
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public int Age { get; set; }
-    }
-}
