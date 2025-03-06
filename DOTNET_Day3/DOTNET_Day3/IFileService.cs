3531ae95468c4b7cc9c2e2f7d3cff8dcaa5c6eb0
Vasu Kansagara <vasu.kansagara@bacancy.com>
2025-03-06 16:23:42 +0530

    Delete DOTNET_Day3 directory

--- DOTNET_Day3/DOTNET_Day3/IFileService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DOTNET_Day3
-{ 
-        public interface IFileService
-        {
-        void SaveStudentsToFile(string filePath, List<Student> students);
-        List<Student> RetriveStudentData();
-    }
-
-}
--- DOTNET_Day3/DOTNET_Day3/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DOTNET_Day3
-{
-    public class Student
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public static List<Student> students = new List<Student>();
-    }
-}
